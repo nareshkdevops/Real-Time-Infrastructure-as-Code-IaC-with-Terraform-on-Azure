ec8e827ee522f9aa5c8ffca8db142f4da9c5e323
nareshkdevops <161805553+nareshkdevops@users.noreply.github.com>
2025-05-11 10:51:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,7 +8,12 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
-  #subscription_id = "" #"enter your subscription id if required." 
+    features {
+      
+    }
+  # Configuration options
+  client_id = "d8ee3aba-48d5-42a2-a12a-669c9e96a856"
+  client_secret = "U1i8Q~x.KfPW7Ifi1h5M7Q5aepyq1SP.XbfZHcyy"
+  tenant_id = "196059ca-34b4-4e7a-88b8-1e86934fd179"
+  subscription_id = "f2e0a72a-bb8f-47bf-946a-c0d4b739dff8"
 }
-
